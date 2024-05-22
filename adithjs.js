34c9083ac78adfc387051b3c061150b2f4d6fe5d
adithcode <150225766+adithcode@users.noreply.github.com>
2024-05-22 15:20:20 +0530

    Add files via upload

---
+++ adithjs.js
@@ -0,0 +1,55 @@
+
+// let age=18;
+// if (age>18){
+//     console.log("you are eligible for driving license")
+// }
+// else{
+//     console.log("you are not eligible")
+// }  
+//
+// let time=20;
+// if (time<=12){
+//     console.log("Good Morning")
+// }
+// else if(time>12 && time<=15){
+//     console.log("Good AfterNoon")
+// }
+// else if (time>15 && time<=19){
+//     console.log("Good Evening")
+// }
+// else{
+//     console.log("Good Night")
+// }
+// let a=20;
+// (a<20)?console.log("a is less than 20"):console.log("a is greater than 20")
+// let i=1
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// let fruits = ["Apple","Orange","Grapes","Banana"]
+// for (let i= 0; i<fruits.length; i++) {
+//     console.log(fruits[i])
+// let fruits = [1,2,3,4,5,6];
+//  for (let i= 0; i<fruits.length; i++) {
+//      if (fruits[i]%2==0)
+//     {
+//         console.log(`${fruits[i]} is even`);
+//     }
+//      else {
+//         console.log(`${fruits[i]} is odd`);
+//     }
+//  }  
+
+//find the  sum of the array
+// let values=[1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// for (let i =0;i<values.length;i++){
+//     sum=sum+values[i];
+// }
+//     console.log(sum);
+//1 to 5
+// for(let i=1;i<5;i++){
+//     for(j=1;j<5;j++){
+//         console.log(`${i} * ${j} = ${i*j}`) 
+//     }
+// }
